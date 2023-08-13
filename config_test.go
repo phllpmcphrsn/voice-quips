9355f00d1950d3aab2f18128223d2376b5827bfd
Phillip <phllpmcphrsn@yahoo.com>
2023-08-13 01:49:15 -0400

    Add config_test.go Ignore audio_files directory

---
+++ config_test.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	log "golang.org/x/exp/slog"
+)
+
+func TestLoadConfig(t *testing.T) {
+	testCases := []struct {
+		name          string
+		configFile    string
+		envVars       map[string]string
+		expectedError bool
+	}{
+		{
+			name:          "ValidConfigFile",
+			configFile:    "testdata/valid_config.yaml",
+			expectedError: false,
+		},
+		{
+			name:          "InvalidConfigFile",
+			configFile:    "testdata/invalid_config.yaml",
+			expectedError: true,
+		},
+		// Add more test cases as needed
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Set environment variables for test case
+			for key, value := range tc.envVars {
+				os.Setenv(key, value)
+				defer os.Unsetenv(key)
+			}
+
+			// Load config
+			_, err := LoadConfig(tc.configFile)
+
+			if tc.expectedError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestHandleCredentials(t *testing.T) {
+	testCases := []struct {
+		name         string
+		store        StorageConfig
+		envVars      map[string]string
+		expectedUser string
+		expectedPass []byte
+	}{
+		{
+			name: "CredentialsEnabled",
+			store: StorageConfig{
+				Credentials: Credentials{
+					Enabled:     true,
+					UserVar:     "TEST_USER",
+					PasswordVar: "TEST_PASS",
+					User:        "defaultUser",
+					Password:    []byte("defaultPass"),
+				},
+			},
+			envVars: map[string]string{
+				"TEST_USER": "testUser",
+				"TEST_PASS": "testPass",
+			},
+			expectedUser: "testUser",
+			expectedPass: []byte("testPass"),
+		},
+		{
+			name: "CredentialsDisabled",
+			store: StorageConfig{
+				Credentials: Credentials{
+					Enabled: false,
+					UserVar: "TEST_USER",
+				},
+			},
+			envVars:      map[string]string{},
+			expectedUser: "defaultUser",
+			expectedPass: []byte("defaultPass"),
+		},
+		// Add more test cases as needed
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Set environment variables for test case
+			for key, value := range tc.envVars {
+				os.Setenv(key, value)
+				defer os.Unsetenv(key)
+			}
+
+			// Handle credentials
+			handleCredentials(&tc.store)
+
+			assert.Equal(t, tc.expectedUser, tc.store.Credentials.UserVar)
+			assert.Equal(t, tc.expectedPass, tc.store.Credentials.Password)
+		})
+	}
+}
+
+func TestPathExists(t *testing.T) {
+	testCases := []struct {
+		name          string
+		path          string
+		expectedError bool
+	}{
+		{
+			name:          "ExistingPath",
+			path:          "testdata",
+			expectedError: false,
+		},
+		{
+			name:          "NonExistingPath",
+			path:          "nonexistent",
+			expectedError: true,
+		},
+		// Add more test cases as needed
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := PathExists(tc.path)
+
+			if tc.expectedError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestGetLogLevel(t *testing.T) {
+	testCases := []struct {
+		name           string
+		logLevel       string
+		expectedOutput log.Level
+	}{
+		{
+			name:           "DebugLogLevel",
+			logLevel:       "debug",
+			expectedOutput: log.LevelDebug,
+		},
+		{
+			name:           "InfoLogLevel",
+			logLevel:       "info",
+			expectedOutput: log.LevelInfo,
+		},
+		{
+			name:           "WarnLogLevel",
+			logLevel:       "warn",
+			expectedOutput: log.LevelWarn,
+		},
+		{
+			name:           "ErrorLogLevel",
+			logLevel:       "error",
+			expectedOutput: log.LevelError,
+		},
+		{
+			name:           "InvalidLogLevel",
+			logLevel:       "invalid",
+			expectedOutput: log.LevelInfo,
+		},
+		// Add more test cases as needed
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			output := GetLogLevel(tc.logLevel)
+			assert.Equal(t, tc.expectedOutput, output)
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,11 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+)
+
+require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
@@ -17,6 +22,7 @@ require (
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.8.4
 	github.com/subosito/gotenv v1.4.2 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
